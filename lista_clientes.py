73775240041314f0d0d928a5376692ecd896fd64
Leandro Lopes Leal <leandrolopesleal26@gmail.com>
2019-07-25 19:10:09 -0300

    Inclusão programas do servidor

---
+++ lista_clientes.py
@@ -0,0 +1,27 @@
+def _cliente(numero):
+    
+    cliente = {} # cria um dicionario vazio para ir adicionando clientes pos
+
+    cliente ['5922'] = ['Caparao','172.20.6.137']
+    cliente ['5920'] = ['Camila','172.20.6.14']
+    cliente ['5808'] = ['Costa Tropical','172.18.30.251']
+    cliente ['5853'] = ['Maracai','172.18.12.250']
+    cliente ['5734'] = ['Riachuelo','172.55.0.17']
+    cliente ['2019'] = ['Maquete','172.20.6.14']
+    cliente ['5410'] = ['Desenvolvimento','172.20.6.143']
+    cliente ['9014'] = ['Central de Monitoramento','172.20.8.2']
+    cliente ['5510'] = ['Teste','172.20.6.143']
+
+
+    for i in cliente:
+
+        if numero == i:
+            
+            c = cliente[i]
+            return c
+
+
+
+
+
+
